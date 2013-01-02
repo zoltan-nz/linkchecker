2537eb7ec426de4dab1fe36644180c4818b8818f
Zoltan Debre <zdebre@gmail.com>
2013-01-02 16:32:52 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkchecker::Application.config.secret_token = '4117ac2113a7c919a66ba8876da8b903cf4cdbeb2c84f3d6f8af73aaaa068e93071b69b684013ae66681c70ed8e1b1807e6949fcab836f68ae2e4a38475a4edb'
